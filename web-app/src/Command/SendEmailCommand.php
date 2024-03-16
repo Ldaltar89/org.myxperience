84e7e3acf8ef0fda4a5b9ead894d7cae9f6d8928
Alexander Samaniego <alexanderwspenafiel@gmail.com>
2024-03-16 01:08:40 -0400

    feat(email): sending emails
    adding message_bus: false on <project>/config/packages/mailer.yaml

--- web-app/src/Command/SendEmailCommand.php
+++ web-app/src/Command/SendEmailCommand.php
@@ -12,42 +12,42 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class SendEmailCommand extends Command
 {
-  protected static $defaultName = 'app:send-test-email';
+    protected static $defaultName = 'app:send-test-email';
 
-  private $mailer;
+    private $mailer;
 
-  public function __construct(MailerInterface $mailer)
-  {
-    parent::__construct();
-    $this->mailer = $mailer;
-  }
-
-  protected function configure()
-  {
-    $this
-      ->setName('app:send-email')
-      ->setDescription('Send a test email');
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-    $output->writeln('Enviando..xx');
-    try {
-      $email = (new Email())
-        ->from('noreply@myxperiences.org')
-        ->to('')
-        ->subject('Test Email')
-        ->text('This is a test email sent via Amazon SES.');
-
-      $this->mailer->send($email);
-
-
-      $output->writeln('Email sent successfully.');
-    } catch (TransportExceptionInterface $e) {
-      $output->writeln('error');
-      $output->writeln($e);
+    public function __construct(MailerInterface $mailer)
+    {
+        parent::__construct();
+        $this->mailer = $mailer;
     }
 
-    return Command::SUCCESS;
-  }
+    protected function configure()
+    {
+        $this
+            ->setName('app:send-email')
+            ->setDescription('Send a test email');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Enviando..xx');
+        try {
+            $email = (new Email())
+                ->from('noreply@myxperiences.org')
+                ->to('')
+                ->subject('Test Email')
+                ->text('This is a test email sent via Amazon SES.');
+
+            $this->mailer->send($email);
+
+
+            $output->writeln('Email sent successfully.');
+        } catch (TransportExceptionInterface $e) {
+            $output->writeln('error');
+            $output->writeln($e);
+        }
+
+        return Command::SUCCESS;
+    }
 }
