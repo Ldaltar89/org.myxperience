45177f85d902cbdbd8e00cde7997d2b83ce0cac0
Alexander Samaniego <alexanderwspenafiel@gmail.com>
2024-03-16 00:51:58 -0400

    feat(email): fixing email sending

--- web-app/src/Controller/RegistrationController.php
+++ web-app/src/Controller/RegistrationController.php
@@ -45,7 +45,9 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
                 (new TemplatedEmail())
                     ->from(new Address('noreply@myxperiences.org', 'My Xperiences'))
                     ->to($user->getEmail())
