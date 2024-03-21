f8d98ecb96c66933587b7741222f401ff97b1311
Leonardo Altamirano <53161185+Ldaltar89@users.noreply.github.com>
2024-03-21 17:26:46 -0500

    feat(desing): finish login, register

--- web-app/src/Form/RegistrationFormType.php
+++ web-app/src/Form/RegistrationFormType.php
@@ -35,7 +35,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', PasswordType::class, ['label'=>'Password',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
